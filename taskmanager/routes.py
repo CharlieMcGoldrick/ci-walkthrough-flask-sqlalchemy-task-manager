7163291c031f5e5974664539a6a5aa09c4bb6957
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-09 16:49:36 +0000

    Feat: add c and r of crud to tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,7 +6,8 @@ from sqlalchemy.exc import IntegrityError
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
 @app.route("/categories")
@@ -18,7 +19,7 @@ def categories():
     # 2nd is the the variable name above that contains the list
     return render_template("categories.html", categories=categories)
 
-
+# Category Routing
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
@@ -36,3 +37,42 @@ def add_category():
             return redirect(url_for("categories"))
     return render_template("add_category.html")
 
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    # Attempts to query the database and if no record is found it will 404
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
+
+# Task Routing
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        try:
+            task = Task(
+                task_name=request.form.get("task_name"),
+                task_description=request.form.get("task_description"),
+                is_urgent=bool(True if request.form.get("is_urgent") else False),
+                due_date=request.form.get("due_date"),
+                category_id=request.form.get("category_id")
+            )
+            db.session.add(task)
+            db.session.commit()
+            flash("Task added successfully!", "success")  # Success message
+            return redirect(url_for("home"))
+        except IntegrityError:
+            db.session.rollback()
+            flash("Task already exists.", "error")  # Error message
+            return redirect(url_for("home"))
+    return render_template("add_task.html", categories=categories)
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
     // sidenav initialization
     let sideNav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sideNav);
+
+    // modal initialization
+    let modalElements = document.querySelectorAll('.modal');
+    M.Modal.init(modalElements);
+
+    // datapicker initialization
+    let datePicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datePicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    // collapsibles initialization
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
