15084bba871ef1ea36d17e0dbc91c735ad762a3a
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-09 14:55:18 +0000

    Feat: add error handling for adding categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 from sqlalchemy.exc import IntegrityError
@@ -17,8 +17,17 @@ def categories():
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
-        category = Category(category_name=request.form.get("category_name"))
-        db.session.add(category)
-        db.session.commit()
-        return redirect(url_for("categories"))
+        # Format the input to handle casing duplicates
+        category_name = request.form.get("category_name").capitalize()
+        try:
+            category = Category(category_name=category_name)
+            db.session.add(category)
+            db.session.commit()
+            flash("Category added successfully!", "success")  # Success message
+            return redirect(url_for("categories"))
+        except IntegrityError:
+            db.session.rollback()
+            flash("Category already exists.", "error")  # Error message
+            return redirect(url_for("categories"))
     return render_template("add_category.html")
+
